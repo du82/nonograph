e1ecbee907d92ede6a790f6238a9151effd918cb
Sam <sgpinkus@gmail.com>
2026-08-23 04:15:54 +0000

    Update Content-Security-Policy img-src, media-src directive to reflect current app behaviour of allowing https? sources. Rm redundant font-src.

--- src/main.rs
+++ src/main.rs
@@ -275,10 +275,10 @@ impl Fairing for SecurityHeadersFairing {
             "Content-Security-Policy",
             "default-src 'self'; \
              base-uri 'self'; \
-             font-src 'self' https: data:; \
              form-action 'self'; \
              frame-ancestors 'self'; \
-             img-src 'self' data:; \
+             img-src 'self' https: http:; \
+             media-src 'self' https: http:; \
              object-src 'none'; \
              script-src 'self' 'unsafe-inline'; \
              script-src-attr 'none'; \
